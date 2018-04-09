00225a1f12130a88fbc913bf7dc84f455b10fb44
Danielle Danskin <danielle.danskin@gmail.com>
2018-04-08 22:11:47 -0400

    calculator 1.0

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  var currentVal = "";
+  updateDisplay();
+  $('#clearAll').on('click',function(){
+    currentVal = "";
+    updateDisplay();
+  });
+  $('#clearLast').on('click',function(){
+    currentVal = currentVal.slice(0,-1);
+    $('#result').html(currentVal);
+  });
+  $(".calc-btn").on('click',function(){
+    currentVal += $(this).attr('value');
+    $('#result').html(currentVal);
+    return currentVal;
+  });
+  
+  $("#total").on('click',function(){
+    currentVal = eval(currentVal);
+    updateDisplay();
+    currentVal = "";
+  });
+  $('#percent').on('click', function(){
+     currentVal = "("+currentVal+")/100";
+     updateDisplay();
+  });
+  function updateDisplay(){
+   
+    $('#result').html(currentVal);
+  }
+});
